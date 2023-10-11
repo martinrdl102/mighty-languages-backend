7f453bc9a0ba9453ff9af47b450b7d9e36875847
Martin Rada <martinradadl@gmail.com>
2023-10-11 18:34:01 -0500

    Courses model modified

--- src/models/courses.js
+++ src/models/courses.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   title: String,
   description: String,
-  lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lesson" }],
-  rating: Number,
   imageURL: String,
 });
 
