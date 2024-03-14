b59745b2324f3317d834c033417bcd27905a4eab
Martin Rada <martinradadl@gmail.com>
2024-03-14 17:04:15 -0500

    helpers file created, question types constants added

---
+++ src/helpers.js
@@ -0,0 +1,9 @@
+const QUESTION_TYPES = {
+    MULT_CHOICE: { id: 'MULT_CHOICE', value: "Opción Múltiple"},
+    FILLING: { id: 'FILLING', value: "Rellenar"},
+}
+
+
+module.exports = {
+    QUESTION_TYPES
+}
