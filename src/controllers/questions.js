8716755b7d8e9847ede819e77b071a9543b55399
Martin Rada <martinradadl@gmail.com>
2024-03-14 17:04:53 -0500

    Question Controller created

---
+++ src/controllers/questions.js
@@ -0,0 +1,58 @@
+const { QUESTION_TYPES } = require("../helpers");
+const questionModel = require("../models/questions");
+
+exports.postQuestion = async (req, res) => {
+  const newQuestion = new questionModel.Question({
+    type: req.body.type,
+    lesson: req.body.lessonId,
+    statements: req.body.statements,
+  });
+  try {
+    const createdQuestion = await newQuestion.save();
+    return res.json(createdQuestion);
+  } catch (e) {
+    console.log(e.message);
+    return res.send("Question not created");
+  }
+};
+
+exports.getQuestionsFromLessonId = async (req, res) => {
+    try {
+        const questions = await questionModel.Question.find({ lesson: req.params.id });
+        return res.json(questions);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+      }
+}
+
+exports.updateQuestion = async (req, res) => {
+    try {
+      const question = await questionModel.Question.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      return res.json(question);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  };
+
+  exports.deleteQuestion = async (req, res) => {
+    try {
+      const question = await courseModel.Course.findByIdAndDelete(req.params.id);
+      return res.json(question);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  };
+
+  exports.getQuestionsTypes = async (req, res) => {
+    try {
+        return res.json(QUESTION_TYPES);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+      }
+}
