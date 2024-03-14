4fd0272208bfc2e969d5dca215e15bd0648525f9
Martin Rada <martinradadl@gmail.com>
2024-03-14 17:05:08 -0500

    Questions model updated

--- src/models/questions.js
+++ src/models/questions.js
@@ -1,23 +1,55 @@
-const mongoose = require("mongoose")
-
-
-
-const choiceTypeSchema = new mongoose.Schema ( {
-    type: String
-})
-
-const OptionType = mongoose.model('OptionType', optionTypeSchema)
+const mongoose = require("mongoose");
+const { QUESTION_TYPES } = require("../helpers");
 
 const schema = new mongoose.Schema({
-    type: String,
-    options: [choiceTypeSchema], 
-    "first-statement": String,
-    "last-statement": String,
-    solution: String,
-    "is-correct": Boolean,
-    "selected-option": String
-})
+  type: {
+    id: {
+      type: String,
+      enum: Object.keys(QUESTION_TYPES),
+    },
+    value: String,
+  },
+  lesson: { type: mongoose.Schema.Types.ObjectId, ref: "Lesson" },
+  statements: [
+    {
+      type: String,
+      value: String,
+      options: [{ value: String, isAnswer: Boolean }],
+    },
+  ],
+});
 
+const Question = mongoose.model("Question", schema);
+module.exports = { Question, schema };
 
-const Question = mongoose.model('Question', schema)
-module.exports = {Question, schema}
+/* 
+    const newQuestion = {
+        type: {id:"MULTI_CHOICE",value:"Opcion multiple"},
+        lesson: "ghghgjhgjhg",
+        statements: [
+            {
+            type: "text", 
+            value: "what is correct?", 
+            options: [
+                { value: "this one is correct", isAnswer: true},
+                { value: "this is wrong", isAnswer: false}
+            ]
+        },
+        {
+            type: "textfield", 
+            value: null, 
+            options: "this one is correct"
+            
+        },
+        {
+            type: "select", 
+            value: null, 
+            options:  [
+                { value: "this one is correct", isAnswer: true},
+                { value: "this is wrong", isAnswer: false}
+            ]
+            
+        },
+        ], 
+    }
+*/
