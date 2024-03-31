0d513eb9d96d22da8acd639c4f0faa0341b541d3
Martin Rada <martinradadl@gmail.com>
2024-03-31 15:15:34 -0500

    Question controller updated

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -2,6 +2,7 @@ const { QUESTION_TYPES } = require("../helpers");
 const questionModel = require("../models/questions");
 
 exports.postQuestion = async (req, res) => {
+  console.log(req.body.type, req.body.lessonId, req.body.statements);
   const newQuestion = new questionModel.Question({
     type: req.body.type,
     lesson: req.body.lessonId,
@@ -17,42 +18,44 @@ exports.postQuestion = async (req, res) => {
 };
 
 exports.getQuestionsFromLessonId = async (req, res) => {
-    try {
-        const questions = await questionModel.Question.find({ lesson: req.params.id });
-        return res.json(questions);
-      } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-      }
-}
+  try {
+    const questions = await questionModel.Question.find({
+      lesson: req.params.id,
+    });
+    return res.json(questions);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
 
 exports.updateQuestion = async (req, res) => {
-    try {
-      const question = await questionModel.Question.findByIdAndUpdate(
-        req.params.id,
-        { $set: req.body },
-        { new: true }
-      );
-      return res.json(question);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  };
+  try {
+    const question = await questionModel.Question.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.json(question);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
-  exports.deleteQuestion = async (req, res) => {
-    try {
-      const question = await courseModel.Course.findByIdAndDelete(req.params.id);
-      return res.json(question);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  };
+exports.deleteQuestion = async (req, res) => {
+  try {
+    const question = await courseModel.Course.findByIdAndDelete(req.params.id);
+    return res.json(question);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
-  exports.getQuestionsTypes = async (req, res) => {
-    try {
-        return res.json(QUESTION_TYPES);
-      } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-      }
-}
+exports.getQuestionsTypes = async (req, res) => {
+  try {
+    return res.json(QUESTION_TYPES);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
