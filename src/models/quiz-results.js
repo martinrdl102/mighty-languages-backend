207b21db11e06722008243fde006160ee4aaddbe
Martin Rada <martinradadl@gmail.com>
2024-04-30 12:46:52 -0500

    Quiz Results model updated

--- src/models/quiz-results.js
+++ src/models/quiz-results.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   lesson: { type: mongoose.Schema.Types.ObjectId, ref: "Lesson" },
+  userAnswers: [{
+    questionIndex: Number,
+    answers: [{ statementIndex: Number, value: String }],
+  }]
 });
 
 const QuizResult = mongoose.model("QuizResult", schema);
